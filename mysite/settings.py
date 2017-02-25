287854bfa5e74f8a47fcbed055aa710248495409
Cherley <you@example.com>
2017-02-25 22:42:12 +0200

    modified template and posts files.

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'cherley2017$default',
+        'USER': 'admin',
+        'PASSWORD': 'cherley2017',
+        'HOST': 'cherley2017.mysql.pythonanywhere-services.com',
     }
 }
 
