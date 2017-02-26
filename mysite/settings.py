2c4cbb4a0c15b48a49e9b7e4922b97855bdd1ca1
Cherley <you@example.com>
2017-02-26 11:38:20 +0200

    added db path.

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': /home/cherley2017/my-first-blog/db.sqlite3
+        'NAME': '/home/cherley2017/my-first-blog/db.sqlite3',
     }
 }
 
